d0f91facb83a00cd990b877c77609a9b1aa69122
silashuereca <silasgranthuereca@gmail.com>
2019-05-22 08:34:14 -0500

    Finished Etch-a-sketch project

--- main.js
+++ main.js
@@ -1,37 +1,75 @@
 
 
+const setGrid = document.getElementById('setGridBtn')
+setGrid.addEventListener('click', changeGrid);
 
-// let colorNumber  = Math.radon * 255;
-// let randomColor = Math.floor(rgb(colorNumber, colorNumber, colorNumber));
-
-
-function generateGrid(){
-for (let i = 0; i < 256; i++) {
-  const row = document.getElementById('etchContainer');
-  let div = document.createElement('div')
-  row.append(div);
-
-  //Changing each div color
-  div.addEventListener('mouseover', changeColor);
-  function changeColor() {
-    div.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
-  }
+function changeGrid() {
+  let gridSize = parseInt(document.getElementById('gridSize').value);
   
-}
+
+
+    function generateGrid(gridSize){
+    let totalGridNum = gridSize * gridSize;
+    
+
+        for (let i = 0; i < totalGridNum; i++) {
+
+
+          //Creating a new div element for grid
+          const row = document.getElementById('etchContainer');
+          let container = document.querySelector('.etch-container');
+          container.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr); grid-template-rows: repeat(${gridSize}, 1fr)`;
+          let div = document.createElement('div')
+          
+          row.append(div);
+
+              //Change each div color to normal black 
+              const blackAndWhiteBtn = document.getElementById('blackAndWhite');
+              blackAndWhiteBtn.addEventListener('click', changeToBlack)
+              
+              function changeToBlack() {
+                div.addEventListener('mouseover', changeColor);
+                function changeColor() {
+                  div.style.backgroundColor = 'black';
+                }
+
+              }
+
+              changeToBlack()
+
+              //Change div color to random rgb value
+              const changeColorBtn = document.getElementById('changeColorBtn');
+              changeColorBtn.addEventListener('click', function(){
+
+                div.addEventListener('mouseover', changeColor);
+              function changeColor() {
+                div.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+              }
+          
+              })
+          
+        }
+    
+
+      }
+
+      generateGrid(gridSize)
 
 }
 
-generateGrid()
+changeGrid()
 
 
-function clearWindow() {
+function clearGrid() {
   const clear = document.getElementById('btn')
   clear.addEventListener('click', function(){
-    window.location.reload(true);
+    window.location.reload(true)
   })
 }
 
-clearWindow();
+clearGrid()
+
+
 
 
 
